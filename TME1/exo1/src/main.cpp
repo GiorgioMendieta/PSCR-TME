82e9b10f23a17dc6a625456d92af45aee6d7df1d
Giorgio <carscars21@gmail.com>
2023-09-22 11:50:05 +0200

    Whitespace changes

--- TME1/exo1/src/main.cpp
+++ TME1/exo1/src/main.cpp
@@ -3,36 +3,39 @@
 #include <iostream>
 #include <cstring>
 
-int main () {
+int main()
+{
 
-	std::string abc = "abc";
-	char * str = new char [3];
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	size_t i = 0;
+  std::string abc = "abc";
+  char *str = new char[3];
+  str[0] = 'a';
+  str[1] = 'b';
+  str[2] = 'c';
+  size_t i = 0;
 
-	if (! strcmp (str, abc.c_str())) {
-		std::cout << "Equal !";
-	}
+  if (!strcmp(str, abc.c_str()))
+  {
+    .std::cout << "Equal !";
+  }
 
-	pr::List list;
-	list.push_front(abc);
-	list.push_front(abc);
+  pr::List list;
+  list.push_front(abc);
+  list.push_front(abc);
 
-	std::cout << "Liste : " << list << std::endl;
-	std::cout << "Taille : " << list.size() << std::endl;
+  std::cout << "Liste : " << list << std::endl;
+  std::cout << "Taille : " << list.size() << std::endl;
 
-	// Affiche à l'envers
-	for (i= list.size() - 1 ; i >= 0 ; i--) {
-		std::cout << "elt " << i << ": " << list[i] << std::endl;
-	}
+  // Affiche à l'envers
+  for (i = list.size() - 1; i >= 0; i--)
+  {
+    std::cout << "elt " << i << ": " << list[i] << std::endl;
+  }
 
-	// liberer les char de la chaine
-	for (char *cp = str ; *cp ; cp++) {
-		delete cp;
-	}
-	// et la chaine elle meme
-	delete str;
-
+  // liberer les char de la chaine
+  for (char *cp = str; *cp; cp++)
+  {
+    delete cp;
+  }
+  // et la chaine elle meme
+  delete str;
 }
