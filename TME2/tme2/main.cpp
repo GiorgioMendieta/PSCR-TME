dd02a6740fe514577f02de1883971fd20edcccc5
Giorgio <carscars21@gmail.com>
2023-09-29 18:38:31 +0200

    Question 2

--- TME2/tme2/main.cpp
+++ TME2/tme2/main.cpp
@@ -5,6 +5,7 @@
 
 #include <vector>
 
+// QUESION 1
 // Pass by reference both the unique word list and the current word
 bool is_new_word(std::vector<std::string> &unique_words, const std::string &word)
 {
@@ -24,6 +25,39 @@ bool is_new_word(std::vector<std::string> &unique_words, const std::string &word
   return true;
 }
 
+// QUESTION 2
+// Pass by reference both the unique word list and the current word
+void count_word_frequency(std::vector<std::pair<std::string, uint>> &word_occurrence, const std::string &word)
+{
+  // For each pair of word & occurrence in word_occurrence vector
+  for (auto &p : word_occurrence)
+  {
+    // If word is found, increment the occurrence count
+    if (p.first == word)
+    {
+      p.second++;
+      return;
+    }
+  }
+  // New word -> Start the count at 0
+  word_occurrence.emplace_back(word, 0);
+}
+
+uint get_word_frequency(std::vector<std::pair<std::string, uint>> &word_occurrence, const std::string &word)
+{
+  // For each pair of word & occurrence in word_occurrence vector
+  for (auto &p : word_occurrence)
+  {
+    // If word is found, increment the occurrence count
+    if (p.first == word)
+    {
+      return p.second;
+    }
+  }
+
+  return 0;
+}
+
 int main()
 {
   using namespace std;
@@ -41,6 +75,7 @@ int main()
   regex re(R"([^a-zA-Z])");
 
   vector<string> unique_words;
+  vector<pair<string, uint>> word_occurrence;
 
   while (input >> word)
   {
@@ -50,15 +85,14 @@ int main()
     transform(word.begin(), word.end(), word.begin(), ::tolower);
     // word est maintenant "tout propre"
 
-    // if (nombre_lu % 100 == 0){
-    //   // on affiche un mot "propre" sur 100
-    //   cout << nombre_lu << ": " << word << endl;
-    // }
-
-    if (is_new_word(unique_words, word))
+    if (nombre_lu % 100 == 0)
     {
-      cout << "New word: " << word << endl;
+      // on affiche un mot "propre" sur 100
+      cout << nombre_lu << ": " << word << endl;
     }
+
+    is_new_word(unique_words, word);
+    count_word_frequency(word_occurrence, word);
 
     nombre_lu++;
   }
@@ -71,8 +105,14 @@ int main()
        << duration_cast<milliseconds>(end - start).count()
        << "ms.\n";
 
+  // QUESTION 1
   cout << "Found a total of " << nombre_lu << " words." << endl;
+  // QUESTION 2
   cout << "Found a total of " << unique_words.size() << " unique words." << endl;
+  // QUESTION 3
+  cout << "'war' was found : " << get_word_frequency(word_occurrence, "war") << " times." << endl;
+  cout << "'peace' was found : " << get_word_frequency(word_occurrence, "peace") << " times." << endl;
+  cout << "'toto' was found : " << get_word_frequency(word_occurrence, "toto") << " times." << endl;
 
   return 0;
 }
