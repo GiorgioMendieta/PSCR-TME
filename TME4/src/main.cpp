56a41df9eefee6a0e7c14509606cfbf78fdd0685
Giorgio <carscars21@gmail.com>
2023-10-13 11:47:47 +0200

    Generate random numbers

--- TME4/src/main.cpp
+++ TME4/src/main.cpp
@@ -1,12 +1,26 @@
 #include "Banque.h"
+#include <cstdlib> // srand, rand
+#include <ctime>   // srand seed
+#include <thread>  // Threads
 
 using namespace std;
 
-const int NB_THREAD = 10;
+const int NB_THREAD = 10;   // N
+const int NB_COMPTES = 100; // K comptes
+const int SOLDEINITIAL = 500;
+
 int main()
 {
   vector<thread> threads;
   // TODO : creer des threads qui font ce qui est demandé
+  srand(time(nullptr)); // Seed for the RNG
+
+  threads.reserve(NB_THREAD);
+  // Generate random account index
+  int i = 1 + rand() / ((RAND_MAX + 1u) / NB_COMPTES);
+  int j = 1 + rand() / ((RAND_MAX + 1u) / NB_COMPTES);
+  // Random amount
+  int m = 1 + rand() / ((RAND_MAX + 1u) / 100);
 
   for (auto &t : threads)
   {
