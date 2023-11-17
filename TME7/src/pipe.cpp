af8e3d3a3be27fe485c5184bb9976fa3360d320d
Giorgio Mendieta <31053658+GiorgioMendieta@users.noreply.github.com>
2023-11-17 17:38:32 +0100

    Finish implementation of named pipe

--- TME7/src/pipe.cpp
+++ TME7/src/pipe.cpp
@@ -24,7 +24,13 @@ int main(int argc, char **argv)
   char **argv1;
   char **argv2;
   int fd[2]; // Used to replace the R/W in the file descriptor table
-  pipe(fd);  // Create new pipe
+
+  // Create new pipe
+  if (pipe(fd) == -1)
+  {
+    perror("Error creating pipe");
+    exit(1);
+  }
 
   // Search until we find the pipe
   int tube_index = 0;
@@ -39,28 +45,48 @@ int main(int argc, char **argv)
   }
 
   // Replace the pipe by a NULL character
-  argv[tube_index][0] = '\0';
+  argv[tube_index] = NULL;
 
   // Create the two sub argument vectors
   argv1 = argv + 1;
-  argv2 = argv + tube_index;
+  argv2 = argv + tube_index + 1;
 
   // First child process (Write)
+  // [0] Std in
+  // [1] Std out  CLOSED, replaced by [3]
+  // [2] Std err
+  //---------------
+  // [3] Ecriture <- Pipe
+  // [4] Lecture  <- Pipe
+
+  // P1 | P2
   if (fork() == 0)
   {
+    std::cout << "Writer: " << argv1[0] << std::endl;
+
     // File_d ancien, File_d nouveau
-    dup2(1, fd[0]);
-    close(fd[1]); // Write end of the pipe
+    close(fd[0]);               // Close write end of the pipe
+    dup2(fd[1], STDOUT_FILENO); // stdout replaced by fd[1]
+
     // Send the program name and the argument vector
     execv(argv1[0], argv1);
   }
 
   // Second child process (Read)
+  // [0] Std in   CLOSED, replaced by [4]
+  // [1] Std out
+  // [2] Std err
+  //---------------
+  // [3] Ecriture <- Pipe
+  // [4] Lecture  <- Pipe
+
+  // P1 | P2
   if (fork() == 0)
   {
+    std::cout << "Reader: " << argv2[0] << std::endl;
     // File_d ancien, File_d nouveau
-    close(fd[0]); // Read end of the pipe
-    dup2(0, fd[1]);
+    close(fd[1]);              // Close read end of the pipe
+    dup2(fd[0], STDIN_FILENO); // stdin replaced by fd[0]
     // Send the program name and the argument vector
     execv(argv2[0], argv2);
   }
